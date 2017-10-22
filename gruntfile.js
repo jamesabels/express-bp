763a4a4bf0873b39c410015c108fb5c33d063d04
James Abels <james@Jamess-MacBook-Pro-2.local>
2017-10-22 18:33:21 -0400

    Add live reload to project

--- gruntfile.js
+++ gruntfile.js
@@ -1,89 +1,86 @@
-module.exports = function(grunt) {
-"use strict";
+module.exports = function (grunt) {
+  "use strict";
 
-require('load-grunt-tasks')(grunt);
+  require('load-grunt-tasks')(grunt);
 
-    grunt.initConfig({
-      sass: {
-        options: {
-          sourceMap: true
-        },
-        dist: {
-          files: {
-            'public/stylesheets/style.css': 'src/sass/style.scss'
-          }
-        }
+  grunt.initConfig({
+    sass: {
+      options: {
+        sourceMap: true
       },
-      autoprefixer: {
-        options: {
-          browsers: ['last 2 versions', 'ie 8', 'ie 9']
-        },
-        no_dest_single: {
-          src: 'public/stylesheets/style.css'
-        },
-      },
-      cssmin: {
-        options: {
-          mergeIntoShorthands: false,
-          roundingPrecision: -1
-        },
-        target: {
-          files: [{
-            // expand: false,
-            // cwd: 'public/stylesheets/',
-            src: ['public/stylesheets/style.css', 'public/stylesheets/!*.min.css'],
-            dest: 'public/stylesheets/style.min.css',
-            ext: '.min.css'
-          }]
-        }
-      },
-      ts: {
-        app: {
-          files: [{
-            src: ["src/ts/\*\*/\*.ts", "!src/ts/.baseDir.ts", "!src/ts/_all.d.ts"],
-            dest: "./dist/"
-          }],
-          options: {
-            rootDir: "./src/ts/",
-            module: "commonjs",
-            noLib: true,
-            target: "es6",
-            sourceMap: false
-          }
-        }
-      },
-      tslint: {
-        options: {
-          configuration: "tslint.json"
-        },
+      dist: {
         files: {
-          src: ["src/\*\*/\*.ts"]
+          'dist/public/stylesheets/style.css': 'src/sass/style.scss'
         }
-      },
-      watch: {
-        ts: {
-          files: ["src/\*\*/\*.ts"],
-          tasks: ["ts", "tslint"]
-        },
-        sass: {
-          files: ["src/sass/*.scss"],
-          tasks: ["sass"]
-        } 
       }
-    });
-  
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-autoprefixer');
-    grunt.loadNpmTasks("grunt-contrib-watch");
-    grunt.loadNpmTasks("grunt-ts");
-    grunt.loadNpmTasks("grunt-tslint");
-  
-    grunt.registerTask("default", [
-      "ts",
-      "tslint",
-      "sass",
-      "autoprefixer",
-      "cssmin"
-    ]);
-  
-  };
+    },
+    autoprefixer: {
+      options: {
+        browsers: ['last 2 versions', 'ie 8', 'ie 9']
+      },
+      no_dest_single: {
+        src: 'dist/public/stylesheets/style.css'
+      },
+    },
+    cssmin: {
+      options: {
+        mergeIntoShorthands: false,
+        roundingPrecision: -1
+      },
+      target: {
+        files: [{
+          src: ['dist/public/stylesheets/style.css', 'dist/public/stylesheets/!*.min.css'],
+          dest: 'dist/public/stylesheets/style.min.css',
+          ext: '.min.css'
+        }]
+      }
+    },
+    ts: {
+      app: {
+        files: [{
+          src: ["src/ts/\*\*/\*.ts", "!src/ts/.baseDir.ts", "!src/ts/_all.d.ts"],
+          dest: "./dist/"
+        }],
+        options: {
+          rootDir: "./src/ts/",
+          module: "commonjs",
+          noLib: true,
+          target: "es6",
+          sourceMap: false
+        }
+      }
+    },
+    tslint: {
+      options: {
+        configuration: "tslint.json"
+      },
+      files: {
+        src: ["src/\*\*/\*.ts"]
+      }
+    },
+    watch: {
+      livereload: {
+        options: { livereload: true },
+        files: ['dist/**/*'],
+      },
+      files: ["src/\*\*/\*.ts", "src/\*\*/\*.scss", 'views/\*\*/\*.jade'],
+      tasks: ["default"]
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-ts");
+  grunt.loadNpmTasks("grunt-tslint");
+  grunt.loadNpmTasks('grunt-livereload');
+
+  grunt.registerTask("default", [
+    "ts",
+    "tslint",
+    "sass",
+    "autoprefixer",
+    "cssmin",
+  ]);
+
+};
--- src/ts/app.ts
+++ src/ts/app.ts
@@ -36,7 +36,7 @@ app.use(require('express-session')({
 app.use(passport.initialize());
 app.use(flash());
 app.use(passport.session());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 
 app.use('/', routes);
